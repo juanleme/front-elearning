e4280ca29c62f2089237e0ea28f31d827b62fa34
Juan Leme <jtlpinheiro@gmail.com>
2016-04-24 14:21:34 -0300

    Videos

--- app/components/videos/videos.controller.js
+++ app/components/videos/videos.controller.js
@@ -9,47 +9,17 @@
 			"com.2fdevs.videogular.plugins.poster",
 			"com.2fdevs.videogular.plugins.buffering"
 		])
+    .controller('VideosController', VideosController);
+	
+	VideosController.$inject = ['$scope', '$sce', '$stateParams', 'VideosService', 'elCookies', 'CLIENT_URL'];
 
-    .controller('VideosController', ['$scope', '$sce', '$stateParams', 'VideosService',
-      function($scope, $sce, $stateParams, VideosService) {
+	function VideosController ($scope, $sce, $stateParams, VideosService, elCookies, CLIENT_URL) {
 			var video = this;
 			video.API = null;
+			video.videos = [];
 
-			video.videos = [
-				{
-					poster: "http://www.videogular.com/assets/images/videogular.png",
-					sources: [
-						{
-							src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), 
-							type: "video/mp4"
-						}
-					]
-				},
-				{
-					poster: "https://i.ytimg.com/vi/0CphTjm7ot8/maxresdefault.jpg",
-					sources: [
-						{
-							src: $sce.trustAsResourceUrl("http://www.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov"), 
-							type: "video/mp4"
-						}
-					]
-				}
-			];
+			activate();
 
-			video.config = {
-				preload: "none",
-				autoHide: false,
-				autoHideTime: 3000,
-				autoPlay: false,
-				sources: video.videos[0].sources,
-				theme: {
-					url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
-				},
-				plugins: {
-					poster: "http://www.videogular.com/assets/images/videogular.png"
-				}
-			};
-			
 			video.onPlayerReady = function(API) {
 				video.API = API;
 			};
@@ -58,8 +28,51 @@
 				video.API.stop();
 				video.config.sources = video.videos[index].sources;
 				video.config.plugins.poster = video.videos[index].poster;
+
 			};
+
+			function listVideos() {
+				var id = $stateParams.id;
+				var key = elCookies.getUser().key;
+
+				VideosService.listVideos(id, key).then(function(data){
+					var i = 0;
+					var r = {};
+
+					for(i = 0; i < data.length; i++){						
+						r = {
+								title : data[i].title,
+								poster: 'assets/img/' + data[i].poster,
+								sources: [
+									{
+										src: $sce.trustAsResourceUrl(CLIENT_URL +'assets/videos/' + data[i].url), 
+										type: 'video/' + data[i].type
+									}
+								]
+							}
+						video.videos.push(r);
+					}
+
+					video.config = {
+						preload: "none",
+						autoHide: false,
+						autoHideTime: 3000,
+						autoPlay: false,
+						sources: video.videos[0].sources,
+						theme: {
+							url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+						},				
+						plugins: {
+							poster: "assets/img/poster/html5-01-poster.png"
+						}
+					};			
+				});
+			}
+			function activate() {
+	    		return listVideos();
+	    	}
+
 			$scope.video = video;
 		}
-    ]);
+
 })();
--- app/components/videos/videos.service.js
+++ app/components/videos/videos.service.js
@@ -1,11 +1,27 @@
 (function() {
-    'use strict'
+    'use strict';
 
     angular.module('el.videos')
+    .factory('VideosService', VideosService);
 
-    .factory('VideosService', function($http) {
+	VideosService.$inject = ['$http', '$log', 'SERVER_URL'];
+
+    function VideosService ($http, $log, SERVER_URL) {
     	return {
-
+        	listVideos: listVideos
     	};
-	});
+
+    	function listVideos(id, token) {
+    		return $http.get(SERVER_URL + 'api/course/'+ id +'/videos?token=' + token)
+	    		.then(getCompleted)
+    			.catch(getFailed);
+    	}
+    	function getCompleted(response) {
+            return response.data;
+    	}
+    	function getFailed(error) {
+    		$log.error('XHR Failed data: '+ error);
+    	}
+	}
+
 })();
